765c4153f171fdeb4a2be76bb9d6ff31e3f9a5f7
Andrew Wong <andrew.j.wong@outlook.com>
2019-11-04 23:00:15 +1100

    Remove dungeons symlink if it exists

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ alias java-fx="/syncDir/UNSW/_COMP2511\ Assignments/H10B-GratefulLatestMegabyte/
 
 ./compile.sh
 
+rm dungeons -f
 ln -s ../H10B-GratefulLatestMegabyte/dungeons/ dungeons
 /syncDir/UNSW/_COMP2511\ Assignments/H10B-GratefulLatestMegabyte/lib/jdk-jfx/bin/java -jar DungeonConsole.jar
 rm dungeons
